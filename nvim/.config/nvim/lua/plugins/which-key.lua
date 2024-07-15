f73fa9cc8bc07d8ce11ae2a3af8faf767f48bc4e
Harsh Patel <patelharsh9797@gmail.com>
2024-07-15 09:34:55 +0530

    Nvim - which-key ui updated

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,6 +1,8 @@
 return {
   "folke/which-key.nvim",
   opts = {
+    ---@type false | "classic" | "modern" | "helix"
+    preset = "helix",
     icons = {
       breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
       separator = "➜", -- symbol used between a key and it's label
@@ -13,8 +15,8 @@ return {
     window = {
       border = "rounded", -- none, single, double, shadow
       position = "bottom", -- bottom, top
-      margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-      padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
+      margin = { 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
+      padding = { 1, 2 }, -- extra window padding [top, right, bottom, left]
       winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
       zindex = 1000, -- positive value to position WhichKey above other floating windows.
     },
