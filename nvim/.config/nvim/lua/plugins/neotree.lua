fa6f2a2e21864928613f2592d0694728fe2b129b
Harsh Patel <patelharsh9797@gmail.com>
2024-07-18 12:11:58 +0530

    neovim neotree.lua added

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,1 +1,24 @@
-return {}
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "right",
+      mappings = {
+        ["Y"] = "none",
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+        },
+        always_show = {
+          ".env",
+          ".env.local",
+        },
+      },
+    },
+  },
+}
