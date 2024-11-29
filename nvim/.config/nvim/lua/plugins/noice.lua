544df4c5a978b9ffa084c3c4824c1d121e88867f
Harsh Patel <patelharsh9797@gmail.com>
2024-11-29 04:31:28 +0000

    nvim: noice.lua updated

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -46,6 +46,14 @@ return {
       opts.presets.lsp_doc_border = true
 
       require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
         presets = {
           bottom_search = false, -- use a classic bottom cmdline for search
           command_palette = true, -- position the cmdline and popupmenu together
