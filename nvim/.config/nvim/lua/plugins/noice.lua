eab9b9bf7a45cf48a79f11b40dab5c155181c870
Harsh Patel <patelharsh9797@gmail.com>
2024-11-29 04:06:10 +0000

    nvim: added mason & noice cmdline position fix

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -46,7 +46,13 @@ return {
       opts.presets.lsp_doc_border = true
 
       require("noice").setup({
-        presets = { inc_rename = true },
+        presets = {
+          bottom_search = false, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
       })
 
       vim.api.nvim_create_autocmd("FileType", {
