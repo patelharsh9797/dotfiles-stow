c829aae34bfacbadbf630c3376222528c2d70a84
Harsh Patel <patelharsh9797@gmail.com>
2024-11-30 11:29:18 +0000

    nvim: lsp diagnostic border added.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -79,6 +79,9 @@ return {
     end,
   },
   {
+    "davidosomething/format-ts-errors.nvim",
+  },
+  {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
@@ -92,7 +95,19 @@ return {
 
       -- LspInfo Border
       require("lspconfig.ui.windows").default_options.border = "rounded"
+      local _border = "rounded"
 
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        border = _border,
+      })
+
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = _border,
+      })
+
+      vim.diagnostic.config({
+        float = { border = _border },
+      })
       opts.inlay_hints = { enabled = false }
 
       opts.servers = {
