27e6325ad43cca324ee0c19ca78f39bbc80d4ef3
Harsh Patel <patelharsh9797@gmail.com>
2024-12-02 06:33:48 +0000

    nvim: completion.lua updated

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,3 +6,7 @@ local opt = vim.opt -- for conciseness
 
 opt.pumblend = 0 -- transperent BG for suggestion and hover docs
 opt.clipboard = "unnamedplus"
+
+-- some transperent effect
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -2,21 +2,29 @@ return {
   {
     "hrsh7th/cmp-nvim-lsp",
   },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-    -- FIX: Remove below code if the snippets not working correctoly
-    opts = {
-      history = true,
-      region_check_events = "InsertEnter",
-      delete_check_events = "TextChanged,InsertLeave",
-    },
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   dependencies = {
+  --     "saadparwaiz1/cmp_luasnip",
+  --     "rafamadriz/friendly-snippets",
+  --   },
+  --   -- Remove below code if the snippets not working correctoly
+  --   opts = {
+  --     history = true,
+  --     region_check_events = "InsertEnter",
+  --     delete_check_events = "TextChanged,InsertLeave",
+  --   },
+  -- },
   {
     "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer", -- source for text in buffer
+      "hrsh7th/cmp-path", -- source for file system paths
+      "L3MON4D3/LuaSnip", -- snippet engine
+      "saadparwaiz1/cmp_luasnip", -- for autocompletion
+      "rafamadriz/friendly-snippets", -- useful snippets
+    },
     opts = function(_, opts)
       opts.window = {
         completion = {
@@ -53,30 +61,44 @@ return {
     end,
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
       require("luasnip.loaders.from_vscode").lazy_load()
+
       cmp.setup({
-        snippet = {
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
+        snippet = { -- configure hiw nvim-cmp interacts with snippet engine
           expand = function(args)
             require("luasnip").lsp_expand(args.body)
           end,
         },
-        window = {
+        window = { -- add border for autocompletion menu
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
+          ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+          ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+          ["<C-e>"] = cmp.mapping.abort(), -- close completion
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
         }),
+        -- sources for autocompletion
         sources = cmp.config.sources({
-          { name = "nvim_lsp" },
           { name = "luasnip" }, -- For luasnip users.
-        }, {
-          { name = "buffer" },
+          { name = "buffer" }, -- text within current buffer
+          { name = "path" }, -- file system paths
         }),
+        -- sources = cmp.config.sources({
+        --   { name = "nvim_lsp" },
+        --   { name = "luasnip" }, -- For luasnip users.
+        -- }, {
+        --   { name = "buffer" },
+        -- }),
       })
     end,
   },
