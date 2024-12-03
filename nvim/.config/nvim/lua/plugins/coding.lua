0c5f5cf6f6319366a52585c13312db2f6bc68f3a
Harsh Patel <patelharsh9797@gmail.com>
2024-12-03 17:17:44 +0000

    nvim: mini.nvim plugin added

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -1,17 +1,36 @@
 return {
-  -- Create annotations with one keybind, and jump your cursor in the inserted annotation
+  -- Comment
+  --
+  -- NORMAL mode
+  -- `gcc` - Toggles the current line using linewise comment
+  -- `gbc` - Toggles the current line using blockwise comment
+  -- `[count]gcc` - Toggles the number of line given as a prefix-count using linewise
+  -- `[count]gbc` - Toggles the number of line given as a prefix-count using blockwise
+  -- `gc[count]{motion}` - (Op-pending) Toggles the region using linewise comment
+  -- `gb[count]{motion}` - (Op-pending) Toggles the region using blockwise comment
+  --
+  -- VISUAL mode
+  -- `gc` - Toggles the region using linewise comment
+  --
+  -- `gb` - Toggles the region using blockwise comment
   {
-    "danymat/neogen",
-    keys = {
-      {
-        "<leader>cc",
-        function()
-          require("neogen").generate({})
-        end,
-        desc = "Neogen Comment",
-      },
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
     },
-    opts = { snippet_engine = "luasnip" },
+    config = function()
+      -- import comment plugin safely
+      local comment = require("Comment")
+
+      local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+      -- enable comment
+      comment.setup({
+        -- for commenting tsx and jsx files
+        pre_hook = ts_context_commentstring.create_pre_hook(),
+      })
+    end,
   },
 
   -- Incremental rename
@@ -70,22 +89,6 @@ return {
       },
     },
     opts = {},
-  },
-
-  -- Go forward/backward with square brackets
-  {
-    "echasnovski/mini.bracketed",
-    event = "BufReadPost",
-    config = function()
-      local bracketed = require("mini.bracketed")
-      bracketed.setup({
-        file = { suffix = "" },
-        window = { suffix = "" },
-        quickfix = { suffix = "" },
-        yank = { suffix = "" },
-        treesitter = { suffix = "n" },
-      })
-    end,
   },
 
   -- Better increase/descrease
--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,16 +1,47 @@
 return {
+  -- Extend and create a/i textobjects
+  { "echasnovski/mini.ai", version = "*", config = true },
+
+  -- Go forward/backward with square brackets
   {
-    "echasnovski/mini.indentscope",
+    "echasnovski/mini.bracketed",
+    event = "BufReadPost",
+    config = function()
+      local bracketed = require("mini.bracketed")
+      bracketed.setup({
+        file = { suffix = "" },
+        window = { suffix = "" },
+        quickfix = { suffix = "" },
+        yank = { suffix = "" },
+        treesitter = { suffix = "n" },
+      })
+    end,
+  },
+
+  -- Highlight patterns in text
+  {
+    "echasnovski/mini.hipatterns",
+    event = "BufReadPre",
     opts = {
-      symbol = "│",
-      options = { try_as_border = true },
-      draw = {
-        animation = function()
-          return 0
-        end,
+      highlighters = {
+        hsl_color = {
+          pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
+          group = function(_, match)
+            local utils = require("solarized-osaka.hsl")
+            --- @type string, string, string
+            local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%?%)")
+            --- @type number?, number?, number?
+            local h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
+            --- @type string
+            local hex_color = utils.hslToHex(h, s, l)
+            return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
+          end,
+        },
       },
     },
   },
+
+  -- Icon provider
   {
     "echasnovski/mini.icons",
     config = function()
@@ -36,66 +67,96 @@ return {
       })
     end,
   },
-  -- {
-  -- 	"lukas-reineke/indent-blankline.nvim",
-  -- 	main = "ibl",
-  -- 	config = function()
-  -- 		local highlight = {
-  -- 			"RainbowRed",
-  -- 			"RainbowYellow",
-  -- 			"RainbowBlue",
-  -- 			"RainbowOrange",
-  -- 			"RainbowGreen",
-  -- 			"RainbowViolet",
-  -- 			"RainbowCyan",
-  -- 		}
-  -- 		local hooks = require("ibl.hooks")
-  -- 		-- create the highlight groups in the highlight setup hook, so they are reset
-  -- 		-- every time the colorscheme changes
-  -- 		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  -- 			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-  -- 			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-  -- 			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-  -- 			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-  -- 			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-  -- 			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-  -- 			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-  -- 		end)
-  --
-  -- 		vim.g.rainbow_delimiters = { highlight = highlight }
-  -- 		require("ibl").setup({
-  -- 			scope = { highlight = highlight },
-  -- 			exclude = { filetypes = { "dashboard", "toggleterm" } },
-  -- 		})
-  --
-  -- 		hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-  -- 	end,
-  -- },
-  -- {
-  -- 	"HiPhish/rainbow-delimiters.nvim",
-  -- 	config = function()
-  -- 		local rainbow_delimiters = require("rainbow-delimiters")
-  --
-  -- 		vim.g.rainbow_delimiters = {
-  -- 			strategy = {
-  -- 				[""] = rainbow_delimiters.strategy["global"],
-  -- 				commonlisp = rainbow_delimiters.strategy["local"],
-  -- 			},
-  -- 			query = {
-  -- 				[""] = "rainbow-delimiters",
-  -- 				lua = "rainbow-blocks",
-  -- 			},
-  -- 			highlight = {
-  -- 				"RainbowDelimiterRed",
-  -- 				"RainbowDelimiterYellow",
-  -- 				"RainbowDelimiterBlue",
-  -- 				"RainbowDelimiterOrange",
-  -- 				"RainbowDelimiterGreen",
-  -- 				"RainbowDelimiterViolet",
-  -- 				"RainbowDelimiterCyan",
-  -- 			},
-  -- 			blacklist = {},
-  -- 		}
-  -- 	end,
-  -- },
+
+  -- Visualize and work with indent scope
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+      draw = {
+        animation = function()
+          return 0
+        end,
+      },
+    },
+  },
+
+  -- Move any selection in any direction
+  {
+    "echasnovski/mini.move",
+    version = "*",
+    event = "VeryLazy",
+    opts = {
+      mappings = {
+        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+        left = "<M-h>",
+        right = "<M-l>",
+        down = "<M-j>",
+        up = "<M-k>",
+
+        -- Move current line in Normal mode
+        line_left = "<M-h>",
+        line_right = "<M-l>",
+        line_down = "<M-j>",
+        line_up = "<M-k>",
+      },
+      -- Options which control moving behavior
+      options = {
+        -- Automatically reindent selection during linewise vertical move
+        reindent_linewise = true,
+      },
+    },
+  },
+
+  -- Text edit operators
+  {
+    "echasnovski/mini.operators",
+    version = "*",
+    opts = {
+      -- Each entry configures one operator.
+      -- `prefix` defines keys mapped during `setup()`: in Normal mode
+      -- to operate on textobject and line, in Visual - on selection.
+
+      -- Evaluate text and replace with output
+      evaluate = {
+        prefix = "g=",
+
+        -- Function which does the evaluation
+
+        func = nil,
+      },
+
+      -- Exchange text regions
+      exchange = {
+        prefix = "gx",
+
+        -- Whether to reindent new text to match previous indent
+        reindent_linewise = true,
+      },
+
+      -- Multiply (duplicate) text
+      multiply = {
+        prefix = "gm", -- gmm : duplicate line
+
+        -- Function which can modify text before multiplying
+        func = nil,
+      },
+
+      -- Sort text
+      sort = {
+
+        prefix = "gs",
+
+        -- Function which does the sort
+        func = nil,
+      },
+    },
+  },
+
+  -- Autopairs
+  { "echasnovski/mini.pairs", version = "*", config = true },
+
+  -- Extend and create a/i textobjects
+  { "echasnovski/mini.surround", version = "*", config = true },
 }
