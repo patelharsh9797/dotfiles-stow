e680e0574f58dfcee3ce1ce2ebb4e0da5e88fa0c
Harsh Patel <patelharsh9797@gmail.com>
2024-12-03 07:27:53 +0000

    nvim: completion file updated

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -15,12 +15,46 @@ return {
   {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
+    keys = {
+      {
+        "<Tab>",
+        function()
+          require("luasnip").jump(1)
+        end,
+        mode = "s",
+      },
+      {
+        "<S-Tab>",
+        function()
+          require("luasnip").jump(-1)
+        end,
+        mode = { "i", "s" },
+      },
+    },
     dependencies = {
       "onsails/lspkind.nvim",
       "hrsh7th/cmp-nvim-lsp", -- source for neovim built-in LSP
       "hrsh7th/cmp-path", -- source for file system paths
       "hrsh7th/cmp-buffer", -- source for text in buffer
-      { "L3MON4D3/LuaSnip", build = "make install_jsregexp" }, -- snippet engine
+      {
+        "L3MON4D3/LuaSnip",
+        lazy = true,
+        build = (not LazyVim.is_win())
+            and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+          or nil,
+        opts = function(_, opts)
+          ---@diagnostic disable-next-line: duplicate-set-field
+          LazyVim.cmp.actions.snippet_forward = function()
+            if require("luasnip").jumpable(1) then
+              require("luasnip").jump(1)
+              return true
+            end
+          end
+
+          opts.history = true
+          opts.delete_check_events = "TextChanged"
+        end,
+      }, -- snippet engine
       "saadparwaiz1/cmp_luasnip", -- for autocompletion
       "rafamadriz/friendly-snippets", -- useful snippets
     },
@@ -57,6 +91,13 @@ return {
           winblend = 0,
         },
       }
+
+      opts.snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      }
+      table.insert(opts.sources, { name = "luasnip" })
 
       -- python specific
       opts.auto_brackets = opts.auto_brackets or {}
@@ -148,13 +189,13 @@ return {
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-          ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+          -- ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+          -- ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
           ["<C-e>"] = cmp.mapping.abort(), -- close completion
-          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
         }),
         -- sources for autocompletion
         sources = cmp.config.sources({
