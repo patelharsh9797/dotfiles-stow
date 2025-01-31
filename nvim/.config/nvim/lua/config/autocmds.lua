1d9cecc36ea527072bc39b104ce0d9ffe51099da
Harsh Patel <patelharsh9797@gmail.com>
2025-01-31 03:51:53 +0000

    nvim: tsx html rainbow color disabled

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,8 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+vim.g.rainbow_delimiters = { query = { tsx = "rainbow-parens", jsx = "rainbow-parens" } }
+
 -- some transperent effect
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
